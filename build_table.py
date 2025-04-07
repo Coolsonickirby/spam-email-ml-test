95b741093c010c5bc68fff128ea4e16e75b5af79
Ali Hussain (Coolsonickirby) <alihussain2001@gmail.com>
2025-04-07 11:23:09 -0400

    Finish build_table script

--- build_table.py
+++ build_table.py
@@ -12,10 +12,37 @@ while True:
 
 source_path = "enron_spam_data.csv"
 
-text = []
+output_messages_file = "dataset.json"
+words_table_file = "words_table.json"
+
+
+words_arr = []
+
+spam_messages = []
+ham_messages = []
 
 with open(source_path, "r", encoding="utf-8") as csvfile:
     data = csv.reader(csvfile, delimiter=',', quotechar='"')
     for row in data:
-        text.append(row[1])
-        text.append(row[2])
+        words = [x.replace("\n", "").strip() for x in row[1].strip().split(" ")]
+        words.extend(x.replace("\n", "").strip() for x in row[2].strip().split(" "))
+        words = list(filter(lambda y: y != "", words))
+        if row[3] == "spam":
+            spam_messages.append(row[2])
+        elif row[3] == "ham":
+            ham_messages.append(row[2])
+        words_arr.extend(words)
+
+# Remove all duplicates
+words_arr = list(set(words_arr))
+
+# Save table
+with open(words_table_file, "w+") as f:
+    json.dump(words_arr, f)
+
+# Save dataset
+with open(output_messages_file, "w+") as f:
+    json.dump({
+        "spam": spam_messages,
+        "ham": ham_messages,
+    }, f, indent=4)
